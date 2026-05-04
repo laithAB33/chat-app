2cccca988533bd6695534fabcb2d24e11980e791
laith <lithtartus@gmail.com>
2026-05-04 20:13:04 +0300

    fix bug

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {verifyGoogleToken} from "../middlewares/authentication.js";
 import {googleAuth} from "../controlers/auth.js";
+import {upload} from "../middlewares/multer.js";
 
 let router = express.Router();
 
-router.post("/google", verifyGoogleToken, googleAuth);
+router.post("/google",upload.none(),verifyGoogleToken, googleAuth);
 
 export {router as authRoutes};
