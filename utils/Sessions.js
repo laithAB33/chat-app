d140e219f125d96c5aeb0df39939d6f0e729f6fe
laith <lithtartus@gmail.com>
2026-09-17 15:35:08 +0300

    fix ttl error

--- utils/Sessions.js
+++ utils/Sessions.js
@@ -4,9 +4,10 @@ import crypto from 'crypto';
 
 let createSession = async function (OldDeviceId,userId, deviceInfo, ip, expiresIn) {
 
-  const expiresAt = Date.now() + expiresIn;
-
+  let  expiresAt = Number(expiresIn) +  Date.now();
+  
   let sid = crypto.randomBytes(32).toString('hex');
+
   let deviceId = OldDeviceId || crypto.randomBytes(32).toString('hex');
 
     const session = new Session({
@@ -20,9 +21,11 @@ let createSession = async function (OldDeviceId,userId, deviceInfo, ip, expiresI
 
     await session.save();
 
-    await redis.hSet(`session:${sid}`, { deviceId:deviceId, userId:String(userId), deviceInfo:deviceInfo || "", ip:ip || "",expiresAt:expiresAt});
 
+    await redis.hSet(`session:${sid}`, { deviceId:deviceId, userId:String(userId), deviceInfo:deviceInfo || "", ip:ip || "",expiresAt:expiresAt});
+    
     await redis.expire(`session:${sid}`, expiresIn / 1000);
+    
     return session;
 }
 
